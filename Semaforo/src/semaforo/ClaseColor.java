389f1e2e29eed34243ca2cf21caa7505e14d071c
RGM73 <101133694+RGM73@users.noreply.github.com>
2022-03-07 18:48:19 +0100

    Add files via upload

---
+++ Semaforo/src/semaforo/ClaseColor.java
@@ -0,0 +1,10 @@
+package semaforo;
+import semaforo.Profesor;
+// Clase color, el profesor pregunta a un alumno por un color entre (rojo, amarillo y verde)
+public class ClaseColor {
+	public static void main(String[] args) {
+		Profesor teacher = new Profesor();
+		String color = teacher.preguntacolor();
+		System.out.println("La respuesta recibida es: " + color);
+	} 
+}
---
+++ Semaforo/src/semaforo/Estudiante.java
@@ -0,0 +1,20 @@
+package semaforo;
+public class Estudiante extends Persona{
+	// Incluye un metodo de clase que se une a los heredados
+	int i_Curso;
+    	public Estudiante() {
+		i_Edad=20;
+		s_Nombre = "Radu Gabriel Maria";
+		i_Curso = 1;
+	}
+	public void presentarse(){
+	System.out.println("Soy " + s_Nombre + " Alumno de " + Integer.toString(i_Curso) + " y 	tengo una edad de: " + Integer.toString(i_Edad));	
+    	}
+	public String preguntacolor(){
+		presentarse();
+		Ordenador mipc = new Ordenador();
+        	return mipc.color();
+    	}
+    
+                
+}
